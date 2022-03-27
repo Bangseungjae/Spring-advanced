ac9831cd0ace4cb583b596a65270db26f0261bc2
Bangseungjae <tmdwo5825@naver.com>
2022-03-27 22:27:50 +0900

    라인 정리

--- src/main/java/hello/advanced/app/v3/OrderRepositoryV3.java
+++ src/main/java/hello/advanced/app/v3/OrderRepositoryV3.java
@@ -1,8 +1,6 @@
 package hello.advanced.app.v3;
 
-import hello.advanced.trace.TraceId;
 import hello.advanced.trace.TraceStatus;
-import hello.advanced.trace.hellotrace.HelloTraceV2;
 import hello.advanced.trace.logtrace.LogTrace;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -31,8 +29,6 @@ public class OrderRepositoryV3 {
             trace.exception(status, e);
             throw e;// 예외를 꼭 다시 던져주어야 한다.
         }
-
-
     }
 
     private void sleep(int millis) {
